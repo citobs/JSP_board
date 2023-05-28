09895da7bb736a4383679f1ac5baa43ea5e40992
citobs <altiorapetamus7@hanmail.net>
2023-05-29 01:06:00 +0900

    jsp 구구단 출력까지

--- src/main/java/org/example/GugudanServlet.java
+++ src/main/java/org/example/GugudanServlet.java
@@ -6,18 +6,26 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import javax.jws.WebService;
 import java.io.IOException;
 
-
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 해석
         resp.setCharacterEncoding("UTF-8"); //완성되는 HTML의 인코딩을 UTF-8로 해석
-        resp.setContentType("text/html; charset-utf-8"); //브라우저에게 우리가 만드는 결과물이 UTF-8 이다.
+        resp.setContentType("text/html;  charset-utf-8"); //브라우저에게 우리가 만드는 결과물이 UTF-8 이다.
 
-        resp.getWriter().append("dsfsdfsddsdfi");
+
+        int dan = Integer.parseInt(req.getParameter("dan"));
+
+//        int i = 1;
+
+        for(int i = 1; i<=9; i++){
+
+
+        resp.getWriter().append("<div>%d * %d단 = %d <div>".formatted(dan, i, dan*i)); //숫자 %d
+
+        }
     }
 }
