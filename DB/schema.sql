b00fcc16aebb7725f85891426d6b51a8a1e9beed
citobs <altiorapetamus7@hanmail.net>
2023-05-29 18:19:30 +0900

    db 스키마 추가

---
+++ DB/schema.sql
@@ -0,0 +1,18 @@
+insert into article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목',
+`body`='내용1';
+
+
+insert into article
+SET regDate = Now(),
+updateDate = NOW(),
+title = '제목2',
+`body`='내용2';
+
+insert into article
+SET regDate = Now(),
+updateDate = NOW(),
+title = '제목3',
+`body`='내용3';
